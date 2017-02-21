68a2d15a3ea8421f62db934cd2fffed507c3522d
sunaemon <@sunaemon0>
2017-02-21 11:01:04 +0900

    update

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-#[macro_use]
-extern crate nom;
-#[macro_use]
-extern crate enum_primitive;
-
-mod types;
-mod parser;
-mod encoder;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+#[macro_use]
+extern crate nom;
+#[macro_use]
+extern crate enum_primitive;
+
 extern crate subprocess;
 use subprocess::*;
-mod lib;
+
+mod types;
+mod parser;
+mod encoder;
 
 fn main() {
     let mut p = Popen::create(&["ps", "x"],
