a733e193edd9b8300bccf4b5239fd71ed60dfa4e
sunaemon <@sunaemon0>
2017-03-25 22:03:23 +0900

    add migration code

---
+++ migrations/20170317050251_create_users/down.sql
@@ -0,0 +1,3 @@
+DROP TABLE kifu;
+
+DROP TABLE users;
---
+++ migrations/20170317050251_create_users/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+  id serial primary key,
+  email varchar not null,
+  hash varchar not null,
+  salt varchar not null,
+  active boolean not null default 'f',
+  balance integer not null default 0
+);
+
+CREATE TABLE kifu (
+  id serial primary key,
+  "user" integer references users(id),
+  data varchar
+)
