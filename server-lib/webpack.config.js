f1889625e37b790f0d1a6ae706f756c702684150
sunaemon <@sunaemon0>
2017-04-11 19:29:39 +0900

    update

---
+++ server-lib/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+const SpritesmithPlugin = require('webpack-spritesmith');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = [{
+    entry: './app/scripts/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            { test: /\.styl$/, loaders: [
+                'style-loader',
+                'css-loader',
+                'stylus-loader'
+            ] },
+            { test: /\.png$/, loaders: [
+                'file-loader?name=i/[hash].[ext]'
+            ] }
+        ]
+    },
+    resolve: {
+        modules: ['node_modules', 'spritesmith-generated']
+    },
+    plugins: [
+        new SpritesmithPlugin({
+            src: {
+                cwd: path.resolve(__dirname, 'app/images/'),
+                glob: '**/*.png'
+            },
+            target: {
+                image: path.resolve(__dirname, 'dist/sprite.png'),
+                css: [[path.resolve(__dirname, 'app/spritesmith-generated/sprite.json'), {
+                    format: 'json_texture'
+                }]
+                ]
+            },
+            apiOptions: {
+                cssImageRef: '~sprite.png'
+            }
+        })
+    ]
+}, {
+    entry: {
+        main: './app/styles/main.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist/'),
+        filename: '[name].css'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({ use: 'css-loader!autoprefixer-loader' })
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({ use: 'css-loader!sass-loader!autoprefixer-loader' })
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css')
+    ]
+}];
